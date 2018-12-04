4c4f14a95b8f3f69779b5722b3442e120887dd9d
Shaun Maharaj <Shaun.Maharaj@elasticpath.com>
2018-12-04 10:06:17 -0500

    Skip element

--- install-google-chrome.sh
+++ install-google-chrome.sh
@@ -208,12 +208,13 @@ function install_missing_dependencies() {
             echo "Finding dependency for ${file}"
 
             if [[ $file == *"libwayland-egl.so.1"* ]]; then
-                package="mesa-libwayland-egl-18.0.5-3.el7.x86_64.rpm"
-            else
-                # Find the package name for this library.
-                package=$(repoquery --repofrompath=centos7,http://mirror.centos.org/centos/7/os/`arch` \
-                    --repoid=centos7 -q --qf="%{name}" --whatprovides "$file")
+                # package="mesa-libwayland-egl-18.0.5-3.el7.x86_64.rpm"
+                continue
             fi
+
+            # Find the package name for this library.
+            package=$(repoquery --repofrompath=centos7,http://mirror.centos.org/centos/7/os/`arch` \
+                --repoid=centos7 -q --qf="%{name}" --whatprovides "$file")
 
             echo "package: ${package}"
 
